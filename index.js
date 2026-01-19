5209a5d31a6d4b0880d66b5a8550972c4677ee7e
leviGatimu <getmorelev@gmail.com>
2026-01-19 13:54:20 +0200

    removed nodes

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const app = express();
+
+app.listen(3001, () => {
+    console.log("Server running on port 3001");
+});
+
+app.use(express.json());
+
+const students = [
+    {
+        id: 1,
+        name: "levi",
+        age: 24,
+        gender: "male",
+        grade: 10
+    },
+    {
+        id: 2,
+        name: "Alpha",
+        age: 25,
+        gender: "male",
+        grade: 10
+    },
+    {
+        id: 3,
+        name: "Tiana",
+        age: 20,
+        gender: "female",
+        grade: 10
+    }
+];
+
+app.get("/students", (req, res) => {
+    res.json(students);
+});
